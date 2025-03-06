73e24f651cee540130fe6c651bf9783ae21e14af
Karlcamp162 <ckarlcamp@gmail.com>
2025-03-07 02:05:55 +0800

    just an update

--- index.rb
+++ index.rb
@@ -1,4 +1,17 @@
-print("Hello World")
-print("Hello World")
-print("Hello World")
+name = "karlrr"
+var = -12893712893.345
+
+puts name.include? "what"
+
+puts name[0,3]
+
+puts name.index("rr")
+puts Math.log(1)
+
+puts var.to_s
+puts var.round
+
+
+something = gets.chomp
+puts "hello " + something + ", anothing shi"
 
